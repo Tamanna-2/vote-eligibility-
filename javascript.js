d2a2cd6ff85b8df2b8c50f62fe495b705f2363e2
Tamanna-2 <152482876+Tamanna-2@users.noreply.github.com>
2024-01-26 19:59:05 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function checkEligibility() {
+    
+    var age = document.getElementById("age").value;
+
+    
+    if (age >= 18) {
+        document.getElementById("result").innerHTML = "You are eligible to vote!";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, you are not eligible to vote yet.";
+    }
+}
